87a26cbc8bb080407007cfe03cdc99e9ef14cb90
paugalles <pau.galles@gmail.com>
2018-07-08 18:39:58 +0200

    50percent

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # translate_json
+
+jupyter notebook --port=8888 --no-browser --ip=0.0.0.0
--- utils.py
+++ utils.py
@@ -3,10 +3,25 @@ from IPython.display import clear_output
 import json
 import copy
 import time
+import numpy as np
+import os,sys
+
+import matplotlib.pyplot as plt
+
+def plot_progress(progress_file = r'C:\Users\bonul\Downloads\translate_json\test\progress.npy'):
+    Ntasks_file = progress_file.replace('progress.npy','Ntasks_file.npy')
+    Ntasks = np.load(Ntasks_file)
+    done_tsks = np.load(progress_file)[1]
+    labels = 'Done', '//TODO'
+    sizes = [done_tsks, Ntasks-done_tsks]
+    explode = (0.1, 0)
+    fig1, ax1 = plt.subplots()
+    aux=ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',shadow=True, startangle=90)
+    aux=ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 def printmd(mystr):
     display(Markdown(mystr))
-
+    
 def getFromDict(dataDict, mapList):    
     for k in mapList: dataDict = dataDict[k]
     return dataDict
@@ -40,7 +55,7 @@ def edit_element(toedit_filename,keys_list,new_v):
     with open(toedit_filename, 'w+',encoding='utf-8') as f:
         json.dump(new_d, f,ensure_ascii=False)
 
-def edition_by_user(v1,v2):
+def edition_by_user(v1,v2,progress_file):
     time.sleep(0.001)
     printmd('# '+v1)
     time.sleep(0.001)
@@ -50,7 +65,15 @@ def edition_by_user(v1,v2):
     if new_v == '':
         new_v = v2
     time.sleep(0.001)
+    plot_progress()
+    time.sleep(0.001)
     clear_output()
+    if new_v=='STOP':
+        np.save( progress_file , [0,np.load(progress_file)[1]] )
+        print('Saving progress and exiting...')
+        time.sleep(3)
+        print('exited')
+        exit
     return new_v
 
 def recursive_dics(d1, d2,toedit_filename,keys_list):
@@ -64,22 +87,33 @@ def recursive_dics(d1, d2,toedit_filename,keys_list):
                 keys_list[-1] = k1
             #
             if type(d1[k1]) is not dict:
-                new_v = edition_by_user(d1[k1],d2[k1])
-                edit_element(toedit_filename,keys_list,new_v)
-                # Reached a dict bottom, reseting keys_list
-                #keys_list = []
-                #return keys_list
+                #
+                progress_file = os.path.join( os.path.dirname(toedit_filename) , 'progress.npy' )
+                curr,target = np.load(progress_file)
+                if curr<target:
+                    Skiper = True
+                    np.save( progress_file , np.load(progress_file)+np.asarray([1,0]) )
+                else:
+                    Skiper = False
+                if not Skiper:
+                    np.save( progress_file , np.load(progress_file)+np.asarray([1,1]) )
+                    new_v = edition_by_user(d1[k1],d2[k1],progress_file)
+                    edit_element(toedit_filename,keys_list,new_v)
             else:
                 recursive_dics(d1[k1], d2[k1],toedit_filename,keys_list)
 
 d1_str = r'C:\Users\bonul\Downloads\translate_json\test\locale-en(1).json'
 d2_str = r'C:\Users\bonul\Downloads\translate_json\test\locale-zh-CN.json'
 
+progress_file = os.path.join( os.path.dirname(d2_str) , 'progress.npy' )
+if not os.path.exists(progress_file):
+    np.save( progress_file , [0,0] )
+
 with open(d1_str, 'rb') as f:
     d1 = json.load(f)
 with open(d2_str, 'rb') as f:
     d2 = json.load(f)
-
+Skiper = True
 recursive_dics(d1, d2,d2_str,[])
 
 aux = """
